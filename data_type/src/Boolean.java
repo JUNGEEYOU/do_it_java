dd4d57823047055408f736a281c269df9438d2bd
jungee yoo <lena@aidenlab.io>
2022-07-22 17:57:59 +0900

    3.자료형 - 2) 불

---
+++ data_type/src/Boolean.java
@@ -0,0 +1,18 @@
+public class Boolean {
+    public static void main(String[] args) {
+        // 예시 1
+        int base = 180;
+        int height = 185;
+        boolean isTall = height > base;
+
+        if(isTall){
+            System.out.println("크다");
+        }
+
+        // 예시 2
+        int i = 3;
+        boolean isOdd = i % 2 == 1;
+        System.out.println(isOdd);  // true
+
+    }
+}
